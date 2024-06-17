138ef83696da046c16032fcf2af0cdc565ad54f0
Belarif <b.ocine@live.fr>
2024-06-17 15:04:44 +0200

    booki-prod : code reorganization

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-const modal = document.querySelector(".modal");
-const modalBtn = document.querySelector("#close-modal");
-
 document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.querySelector(".modal");
+  const modalBtn = document.querySelector("#close-modal");
+
   modalBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
